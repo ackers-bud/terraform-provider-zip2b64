01054d8fe1ae444a372717e211c4b8a0b14e3db5
Mark Ackroyd <mark.ackroyd@thisisbud.com>
2022-11-23 09:13:13 +0000

    Fixing issues for Terraform release

--- examples/main.tf
+++ examples/main.tf
@@ -7,7 +7,7 @@ resource "http-b64" "provisioningzipfile" {
 
 
 resource "zip2b64" "extractedfile" {
-  provider = zip2b64
+  provider   = zip2b64
   base64file = http-b64.provisioningzipfile.response_body_base64
-  filename = "/ca.crt"
+  filename   = "/ca.crt"
 }
